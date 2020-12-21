6cdc644eda2f8f80c62d3c8213671a7866d4cd0d
Chris Dzombak <chris@dzombak.com>
2020-12-21 18:59:09 -0500

    refactor

--- music_dir_tree.go
+++ music_dir_tree.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+
+	"github.com/Bios-Marcel/wastebasket"
 )
 
 // TODO(cdzombak): normalize BaseNameNormalized to lowercase only if destination is a case insensitive filesystem, or allow customizing
@@ -131,4 +133,40 @@ func (n *MusicTreeNode) Walk(callback func(n *MusicTreeNode) error) error {
 		}
 	}
 	return callback(n)
+}
+
+// RemoveChildrenMatching will remove any child nodes _and the filesystem objects they represent_ for which
+// the given removeMatchFunc returns true.
+// Returns the number of nodes removed, and an error if one is encountered.
+func (n *MusicTreeNode) RemoveChildrenMatching(removeMatchFunc func(n *MusicTreeNode) bool, logReason string) (int, error) {
+	removeCount := 0
+	if n.Children != nil {
+		for childKey, childNode := range n.Children {
+			count, err := childNode.RemoveChildrenMatching(removeMatchFunc, logReason)
+			removeCount += count
+			if err != nil {
+				return removeCount, err
+			}
+
+			if removeMatchFunc(childNode) {
+				delete(n.Children, childKey)
+
+				if !*dryRunFlag {
+					if *verboseFlag {
+						log.Printf("Removing '%s' because %s.", childNode.FilesystemPath, logReason)
+					}
+					if err := wastebasket.Trash(childNode.FilesystemPath); err != nil {
+						return removeCount, fmt.Errorf("failed to trash '%s': %w", childNode.FilesystemPath, err)
+					}
+					removeCount++
+				} else {
+					removeCount++
+					if *verboseFlag {
+						log.Printf("[dry run] Would remove '%s' because %s.", childNode.FilesystemPath, logReason)
+					}
+				}
+			}
+		}
+	}
+	return removeCount, nil
 }
